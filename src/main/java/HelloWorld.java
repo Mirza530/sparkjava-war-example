0ae7123e3a9e88666ab3d1735337fff52bf7fb46
Mirza530 <mirzatheprofessional@gmail.com>
2026-03-15 23:01:53 +0530

    Update HelloWorld.java bug fixed

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,14 +1,14 @@
-//import spark.servlet.SparkApplication;
+import spark.servlet.SparkApplication;
 
-//import static spark.Spark.get;
+import static spark.Spark.get;
 
-//public class HelloWorld implements SparkApplication {
-	//public static void main(String[] args) {
+public class HelloWorld implements SparkApplication {
+	public static void main(String[] args) {
 		//new HelloWorld().init();
 	}
 
 	@Override
-	//public void init() {
-		//get("/hello", (req, res) -> "Hello World");
+	public void init() {
+		get("/hello", (req, res) -> "Hello World");
 	}
 }
